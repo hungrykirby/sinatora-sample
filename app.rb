1e4b7d16034a00444e8de81c7dd2bb7083d10772
katsuki hanawa <katsuki.hanawa@nomal.jp>
2019-01-11 18:03:04 +0900

    add work directry

--- app.rb
+++ app.rb
@@ -7,3 +7,17 @@ get '/' do
   @is_top = true
   erb :index
 end
+
+get '/works/*' do |worksname|
+  @title = 'きゃりかつルーム'
+  @name = worksname + 'という名前の作品は見つかりませんでした!'
+  erb :works
+end
+
+not_found do
+  "Whoops! You requested a route that wasn't available."
+end
+
+error do
+  "Y U NO WORK?"
+end
